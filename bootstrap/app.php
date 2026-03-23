5d4d1cd9166461942fd80521b453eba1f8648791
Abbas60-code <muhammadabbas09dec@gmail.com>
2026-03-23 17:50:18 +0500

    Remove HandleCors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\HandleCors::class);
         $middleware->validateCsrfTokens(except: [
             'contact',
             '/contact',
