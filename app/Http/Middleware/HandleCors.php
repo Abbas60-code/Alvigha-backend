680dfceec5622d8aa40967a76db942238e22cf03
Abbas60-code <muhammadabbas09dec@gmail.com>
2026-03-27 01:13:37 +0500

    Fix CORS preflight handling and update middleware

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -18,6 +18,17 @@ class HandleCors
 
         $origin = $request->headers->get('Origin');
 
+        if ($request->isMethod('OPTIONS')) {
+            $response = response('', 204);
+            if (in_array($origin, $allowedOrigins)) {
+                $response->headers->set('Access-Control-Allow-Origin', $origin);
+            }
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+            return $response;
+        }
+
         $response = $next($request);
 
         if (in_array($origin, $allowedOrigins)) {
