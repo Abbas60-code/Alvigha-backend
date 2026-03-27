264063949e5c8f270610ed43d994eb3ac5911498
Abbas60-code <muhammadabbas09dec@gmail.com>
2026-03-27 16:28:56 +0500

    fix: cors middleware updated

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -9,35 +9,11 @@ class HandleCors
 {
     public function handle(Request $request, Closure $next)
     {
-        $allowedOrigins = [
-            'http://localhost:5173',
-            'http://localhost:3000',
-            'http://127.0.0.1:5173',
-            'https://alvigha-frontend.vercel.app',
-        ];
-
-        $origin = $request->headers->get('Origin');
-
-        if ($request->isMethod('OPTIONS')) {
-            $response = response('', 204);
-            if (in_array($origin, $allowedOrigins)) {
-                $response->headers->set('Access-Control-Allow-Origin', $origin);
-            }
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
-            $response->headers->set('Access-Control-Allow-Credentials', 'true');
-            return $response;
-        }
-
         $response = $next($request);
 
-        if (in_array($origin, $allowedOrigins)) {
-            $response->headers->set('Access-Control-Allow-Origin', $origin);
-        }
-
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
         return $response;
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\HandleCors::class);
+        $middleware->prepend(\App\Http\Middleware\HandleCors::class);
         $middleware->validateCsrfTokens(except: [
             'contact',
             '/contact',
